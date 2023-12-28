203980a869ec289ff01e556f0f3c9b63d41cc438
Haitao Wu <haitaowu14@gmail.com>
2023-12-28 21:05:10 +0100

    Cambios

--- src/main/java/com/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/var")
-public class PathVariableController {
+public class PathVariableController { 
 
     @PostMapping("/create")
     public User create(@RequestBody User user){
